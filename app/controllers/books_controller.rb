2f448e53e7f0a38def81bc4966079d84fbdc6ee5
mao-tk <126842098+mao-tk@users.noreply.github.com>
2023-04-05 07:00:33 +0000

    [Add]投稿機能（フォーム途中）

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,12 @@
 class BooksController < ApplicationController
   def index
-    @user = User.find(params[:id])
+    @book = Book.new
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to book_path(params[:id])
   end
 
   def show
@@ -8,4 +14,11 @@ class BooksController < ApplicationController
 
   def edit
   end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root to: "homes#top"
   devise_for :users
+  root to: "homes#top"
   
   get 'home/about' => "homes#about", as: "about"
   
