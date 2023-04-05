092646b7fd95fd45f8de8c6465be7f3dc49b8855
mao-tk <126842098+mao-tk@users.noreply.github.com>
2023-04-05 01:46:10 +0000

    [Add]userモデルに画像を保存できるようにした

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+  
+  has_one_attached :profile_image
+  
   has_many :books, dependent: :destroy
+  
+  def get_profile_image(width, height)
+    unless profile_image.atthached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default_image', content_type: 'image/jpg')
+    end
+    profile_image.variant(resize_to_limit:[width, height]).processed
+  end
   
 end
