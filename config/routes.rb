4ffd4ea6ee350cd6c2b483974cb9e33b16b11789
mao-tk <126842098+mao-tk@users.noreply.github.com>
2023-04-05 04:08:37 +0000

    [Add]ヘッダー・フッター

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   root to: "homes#top"
+  devise_for :users
+  
   get 'home/about' => "homes#about", as: "about"
   
   resources :books, only: [:index, :show, :edit]
   
   resources :users, only: [:index, :show, :edit]
   
-  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -54,6 +54,8 @@ ActiveRecord::Schema.define(version: 2023_04_05_003107) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name"
+    t.string "introduction"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
