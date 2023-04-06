b22bb12151fd142c3a79a026c134789ee9682acc
mao-tk <126842098+mao-tk@users.noreply.github.com>
2023-04-06 06:36:04 +0000

    [Add]userinfo/index.show.edit(book)/index.show(user)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,24 +1,42 @@
 class BooksController < ApplicationController
   def index
+    @user = current_user
     @book = Book.new
+    @books = Book.all
   end
-  
+
   def create
-    @book = Book.new(book_params)
+    @book = current_user.books.new(book_params)
     @book.save
-    redirect_to book_path(params[:id])
+    redirect_to book_path(@book.id)
   end
 
   def show
+    @user = User.find(params[:id])
+    @book = Book.new
+    @book_show = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
   
-  private
+  def update
+    book = Book.find(params[:id])
+    book.update
+    redirect_to book_path(book.id)
+  end
   
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  private
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
   def index
+    @user = current_user
+    @users = User.order(id: :desc)
   end
 
   def show
+    @book = Book.new
     @user = User.find(params[:id])
-    
+    @books = @user.books
   end
 
   def edit
