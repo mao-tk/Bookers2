fcb49261e896127ccd09c0b68a775215dd72a49f
mao-tk <126842098+mao-tk@users.noreply.github.com>
2023-04-07 01:10:05 +0000

    [Add]user edit/update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,13 @@ class UsersController < ApplicationController
     @users = User.order(id: :desc)
     @book = Book.new
   end
-  
+
   def create
     @book = current_user.books.new(book_params)
     @book.save
     redirect_to book_path(@book.id)
   end
-  
+
   def show
     @book = Book.new
     @user = User.find(params[:id])
@@ -20,5 +20,17 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
-  
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
 end
